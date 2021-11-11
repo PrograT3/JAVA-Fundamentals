63fecbf71d40c7b9f7c8a756ee6faad37b596b74
sectorcode <josue.lopez.acosta@sectorcode.com>
2021-11-10 21:09:59 -0500

    commit inicial

---
+++ Lab01/src/lab01/Class03.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab01;
+
+/**
+ *
+ * @author Josue
+ */
+public class Class03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+        Class01 a01 = new Class01();  /// Instanciamiento  de clase
+        Class02 a02 = new Class02();
+        
+        a01.main(null);
+        a02.main(null);
+        
+    }
+    
+}
